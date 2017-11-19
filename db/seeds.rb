3c583550c60313fb05dd6e46254ba6676ddf282c
raphael-theron <raphael.theron@gmail.com>
2017-11-19 16:15:20 +0100

    Display a list of dinosaurs

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+dino4 = Dinosaur.create!( { name: "Tyranosaurus", age: rand(1..100), image_url: "https://goo.gl/EqRlgP" } )
+dino5 = Dinosaur.create!( { name: "Diplodocus", age: rand(1..100), image_url: "https://goo.gl/EqRlgP" } )
